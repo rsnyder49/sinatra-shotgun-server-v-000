aa0a71cc285bfef86204436706e5ef8e2fecd36e
Robert Snyder <r.snyder49@gmail.com>
2018-11-05 20:38:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! 
-    I built this
-    and more lines"
+    "This is shotgun"
   end
 
 end
